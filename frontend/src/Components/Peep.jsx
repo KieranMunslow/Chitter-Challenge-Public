128aba2cca29003312a3da013b914c286893fb46
Kieran Munslow <kieranmunslow@icloud.com>
2021-12-08 19:47:23 +0000

    Wrote code to pass the fourth failing test

--- frontend/src/Components/Peep.jsx
+++ frontend/src/Components/Peep.jsx
@@ -3,7 +3,10 @@ const Peep = ({ peep }) => {
     const { name, username, date, message } = peep;
 
     return (
-        <h6>{name} <span>@{username}</span> <span>- {date}</span></h6>
+        <>
+            <h6>{name} <span>@{username}</span> <span>- {date}</span></h6>
+            <p>{message}</p>
+        </>
     );
 };
 
